ebb9d592f335b5eb3e2fb0978afd2ab242b9ed21
bear90 <mikhail.soza@inspirativ.com>
2014-02-13 11:31:54 +0300

    update one-height-grid for julia

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,57 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
 	  	clean: ['dist'],
-				
-		requirejs: {
-			
-			build: {
-			
-				options: {
-					
-					paths: {
-						'backbone': '../vendor/backbone-min',
-						'underscore': '../vendor/underscore-min',
-						'jquery': '../vendor/jquery-1.9.1.min'
-					},
-					
-					baseUrl: 'src',
-			    	
-			    	name: 'one-height-grid',
-			    	
-			    	out: 'dist/one-height-grid.js',
-			    	
-			    	exclude: ['backbone', 'underscore', 'jquery']
-				}
-		
-			}
-		}
+        
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'qunit']
+        }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', []);
+
+  grunt.registerTask('default', ['concat', 'uglify']);
   
-  grunt.registerTask('default', ['clean', 'requirejs']);
 };
